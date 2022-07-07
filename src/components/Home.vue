465abb9c23a86391ecafa9179920f6e19cda4458
Jesus <jesusfriverar@gmail.com>
2022-07-06 20:26:33 -0400

    Inicio

---
+++ src/components/Home.vue
@@ -0,0 +1,40 @@
+<template>
+    <Layout>
+        <template #header>
+            <Header></Header>
+        </template>
+        <template #resume>
+            <Resume
+                :total-label="'Ahorro total'"
+                :label="label"
+                :total-amount="1000000"
+                :amount="amount"
+            />
+        </template>
+        <template #movements>
+            <Movements/>
+        </template>
+    </Layout>
+</template>
+
+<script>
+import Layout from "./Layout.vue";
+import Header from "./Header.vue";
+import Resume from "./Resume/Index.vue";
+import Movements from "./Movements.vue";
+
+export default {
+    components: {
+        Layout,
+        Header,
+        Resume,
+        Movements,
+    },
+    data() {
+        return {
+            label: null,
+            amount: null,
+        }
+    },
+};
+</script>
---
+++ src/components/Movements.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="movements">
+        movements        
+    </div>
+</template>
+
+<style scoped>
+.movements {
+  max-height: 100%;
+  padding: 0 8px;
+  margin-bottom: 14px;
+}
+
+.title {
+  margin: 8px 16px 24px 16px;
+  color: var(--brand-blue);
+}
+
+.content {
+  max-height: 68vh;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  overflow-y: scroll;
+}
+</style>
